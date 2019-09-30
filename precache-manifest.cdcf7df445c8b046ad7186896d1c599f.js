e40cd6026f1ab2c0dbf1aab1fc4ad53fe9251f29
Jacob <jkabshear3@gmail.com>
2019-09-29 20:21:48 -0400

    Updates

---
+++ precache-manifest.cdcf7df445c8b046ad7186896d1c599f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "28d2f46e6629552f9a02ec00ad51bb1f",
+    "url": "/find-any-city-time/index.html"
+  },
+  {
+    "revision": "39e2199e86616dc1c02f",
+    "url": "/find-any-city-time/static/css/main.b7caddd8.chunk.css"
+  },
+  {
+    "revision": "6a7b5e9fd31048eb2fec",
+    "url": "/find-any-city-time/static/js/2.234cf611.chunk.js"
+  },
+  {
+    "revision": "39e2199e86616dc1c02f",
+    "url": "/find-any-city-time/static/js/main.3d6f9e43.chunk.js"
+  },
+  {
+    "revision": "63e5125a540855b3a9b9",
+    "url": "/find-any-city-time/static/js/runtime~main.f3aefe66.js"
+  },
+  {
+    "revision": "ca381f8c2397b6b36decff89e04d16ec",
+    "url": "/find-any-city-time/static/media/global.ca381f8c.jpg"
+  },
+  {
+    "revision": "5c93a7d9a3d228bb494ee144d230f415",
+    "url": "/find-any-city-time/static/media/loadGlobe.5c93a7d9.gif"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/find-any-city-anytime/precache-manifest.af900adf00d7d893bf04419b6b8a074b.js"
+  "/find-any-city-time/precache-manifest.cdcf7df445c8b046ad7186896d1c599f.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/find-any-city-anytime/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/find-any-city-time/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
